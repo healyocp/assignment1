dd15b47e57c4617b6767d55a445011fcfafc07e7
Portia Healy O'Connor <portiahealyoconnor@Portias-MacBook.local>
2018-10-01 10:34:00 +0100

    finsihing bt

--- src/csassignment/LCA.java
+++ src/csassignment/LCA.java
@@ -90,6 +90,89 @@ private Node put (Node node, Key key, Value val){
 	return node;
 }
 
+/**
+ * Deletes a key from a tree (if the key is in the tree).
+ * Note that this method works symmetrically from the Hibbard deletion:
+ * If the node to be deleted has two child nodes, then it needs to be
+ * replaced with its predecessor (not its successor) node.
+ *
+ * @param key the key to delete
+ */
+public void delete(Key key) {
+	root = delete(root, key);	
+}
+
+private Node delete (Node node, Key key) {
+	if (node == null) { 
+		return null;
+	}
+	
+    int compare = key.compareTo(node.key);
+    
+    if   (compare > 0) {
+    	node.right = delete(node.right, key);
+    	node.left  = delete(node.left,  key);
+    }
+    else if (compare < 0) {
+    	node.left  = delete(node.left,  key);
+    }
+    else {
+    	if (node.right == null) {
+    		return node.left;
+    	}
+        if (node.left  == null) {
+        	return node.right;
+        }
+        Node temp = node;
+        node = max(temp.left);                              
+        node.left = deleteMax(temp.left);                 
+        node.right = temp.right; 
+    }
+    
+    node.N = size(node.left) + size(node.right) + 1;
+    return node;
+	
+}
+
+private Node deleteMax(Node node) 
+{
+    if (node.right == null) return node.left;
+    node.right = deleteMax(node.right);
+    node.N = size(node.left) + size(node.right) + 1;                                 
+    return node;
+}
+
+public Node max(Node node)
+   {
+     if(node.right!=null) {
+       return max(node.right);
+     }
+     return node;
+   }
+
+// Used: www.geeksforgeeks.org/lowest-common-ancestor-in-a-binary-search-tree as a help source.
+ 	public Key lowestCommonAncestor (Node node, Key key1, Key key2){
+ 		if (node == null)
+             return null;
+ 		if (node.key == key1) {
+ 			return node.key;
+ 		}
+ 		if (node.key == key2) {
+ 			return node.key;
+ 		}
+ 		int cmp1 = node.key.compareTo(key1);
+ 		int cmp2 = node.key.compareTo(key2);
+ 		
+         if (cmp1 >= 0 && cmp2 >= 0)
+             return lowestCommonAncestor(node.left, key1, key2);
+   
+         if (cmp1 <= 0 && cmp2 <= 0)
+             return lowestCommonAncestor(node.right, key1, key2);
+   
+         return node.key;
+ 	}
+
+
 
 
 
